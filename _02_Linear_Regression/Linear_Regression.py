976672bce7d85b74cb9311c49277bb9f12de6b84
smilekinging <80910999+smilekinging@users.noreply.github.com>
2023-05-03 22:25:27 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -17,7 +17,7 @@ def ridge(data):
 def lasso(data):
     label = 2e-5
     x,Y = read_data()
-    weight = array(np.eye(6))
+    weight = array([1,1,1,1,1,1])
     y = np.dot(weight,x)
     loss = (np.sum(y - Y)**2) / 6
     rate = 1e-10
