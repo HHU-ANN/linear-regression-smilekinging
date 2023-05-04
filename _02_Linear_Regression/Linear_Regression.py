23ecf70276e2e15e080ab23a3044e392f36d17ec
smilekinging <80910999+smilekinging@users.noreply.github.com>
2023-05-05 00:49:12 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -15,27 +15,17 @@ def ridge(data):
     return weight @ data
     
 def lasso(data):
-    label = 2e-8
-    m = 6
-    x,Y = read_data()
-    weight = np.ones([1,6])
-    #weight = np.zeros(6)
-    #weight = weight.reshape(-1,1)
-    y = np.dot(weight,x.T)
-    lambd = 0.5
-    loss = (np.sum(y - Y)**2) / 6 + lambd * np.linalg.norm(y-Y,ord = 1) / 12
-    dw = np.dot((y - Y),x)
-    #dw = np.dot(y - Y, x) / n + lambd * np.sign(weight)
-    rate = 1e-10
+    x, Y = read_data()
+    weight = data
+    y = np.dot(weight, x.T)
+    lambd = 3000
+    rate = 0.00000000086
     for i in range(int(2e5)):
         y = np.dot(weight, x.T)
-        loss = (np.sum(y - Y) ** 2) / 6 + lambd * np.linalg.norm(y-Y,ord = 1) / 12
-        if abs(loss) < label:
-            break
-        dw = np.dot((y - Y),x)
-        #dw = np.dot(y - Y, x) / n + lambd * np.sign(weight)
-        weight = weight * ( 1 - (rate * lambd / 6)) - dw * rate
-        weight = weight - rate * dw
+        # dw = np.dot((y - Y), x)
+        dw = np.dot(y - Y, x) + lambd * np.sign(weight)
+        weight = weight * (1 - (rate * lambd / 6)) - dw * rate
+        # weight = weight - rate * dw
     return weight @ data
 
 def read_data(path='./data/exp02/'):
